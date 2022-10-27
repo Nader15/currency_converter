504c016d24b98f1a17c8342829caf41873a6fdea
Nader <nadersalah20201111@gmail.com>
2022-10-27 15:28:47 +0200

    Countries Flags Added

--- lib/Logic/controllers/home_controller.dart
+++ lib/Logic/controllers/home_controller.dart
@@ -69,6 +69,7 @@ class HomeController extends GetxController {
       }
     } finally {
       isLoading(false);
+      searchKey.value = '';
     }
   }
 
--- lib/View/Screens/Home_Screen/Select_Currency_Screen/select_currency_screen.dart
+++ lib/View/Screens/Home_Screen/Select_Currency_Screen/select_currency_screen.dart
@@ -1,6 +1,7 @@
 import 'package:currency_converter/Logic/controllers/home_controller.dart';
 import 'package:currency_converter/Utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import 'Widgets/search_widget.dart';
@@ -15,19 +16,20 @@ class SelectCurrencyScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Select Currency"),
         centerTitle: true,
-        // bottom: PreferredSize(
-        //   preferredSize: const Size.fromHeight(60),
-        //   child: Padding(
-        //     padding: const EdgeInsets.only(right: 10, left: 10, bottom: 10),
-        //     child: SearchTextField(),
-        //   ),
-        // ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 10, left: 10, bottom: 10),
+            child: SearchTextField(),
+          ),
+        ),
       ),
       body: ListView.builder(
         physics: const BouncingScrollPhysics(),
         itemCount: homeController.currenciesList.length,
         itemBuilder: (BuildContext context, int index) {
           return Obx(() {
+            var country = "${homeController.currenciesList[index].currency[0].toLowerCase()}${homeController.currenciesList[index].currency[1].toLowerCase()}";
             if (homeController.searchKey.value.isEmpty) {
               return ListTile(
                 onTap: () async {
@@ -49,24 +51,42 @@ class SelectCurrencyScreen extends StatelessWidget {
                   Get.back();
                 },
                 tileColor: AppColors.WHITE_COLOR,
-                leading: Text(
-                  homeController.currenciesList[index].currency,
-                  style: const TextStyle(fontSize: 15),
+                leading: FadeInImage.assetNetwork(
+                  placeholder: "assets/images/placeholder.jpg",
+                  // fit: BoxFit.cover,
+                  // height: 15,
+                  width: 40,
+                  fit: BoxFit.cover,
+                  image: "https://flagcdn.com/w20/$country.png",
+                  imageErrorBuilder: (c, o, s) => Image.asset(
+                    "assets/images/placeholder.jpg",
+                    // fit: BoxFit.cover,
+                    height: 30,
+                    width: 40,
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 title: Text(
                   homeController.currenciesList[index].country,
                   style: const TextStyle(fontSize: 10),
                 ),
+                trailing: Text(
+                  homeController.currenciesList[index].currency,
+                  style: const TextStyle(fontSize: 15),
+                ),
+
               );
-            }
-            else{
+            } else {
               if (homeController.currenciesList[index].country
-                  .toString()
-                  .toLowerCase()
-                  .startsWith(homeController.searchKey.value.toLowerCase())||homeController.currenciesList[index].currency
-                  .toString()
-                  .toLowerCase()
-                      .startsWith(homeController.searchKey.value.toLowerCase())){
+                      .toString()
+                      .toLowerCase()
+                      .startsWith(
+                          homeController.searchKey.value.toLowerCase()) ||
+                  homeController.currenciesList[index].currency
+                      .toString()
+                      .toLowerCase()
+                      .startsWith(
+                          homeController.searchKey.value.toLowerCase())) {
                 return ListTile(
                   onTap: () async {
                     if (homeController.firstCountrySelected.value) {
@@ -84,18 +104,33 @@ class SelectCurrencyScreen extends StatelessWidget {
                     homeController.firstCountrySelected(false);
                     homeController.secondCountrySelected(false);
                     await homeController.convertCurrencies();
-                    homeController.searchKey.value = '';
                     Get.back();
                   },
                   tileColor: AppColors.WHITE_COLOR,
-                  leading: Text(
-                    homeController.currenciesList[index].currency,
-                    style: const TextStyle(fontSize: 15),
+                  leading: FadeInImage.assetNetwork(
+                    placeholder: "assets/images/placeholder.jpg",
+                    // fit: BoxFit.cover,
+                    // height: 15,
+                    width: 40,
+                    fit: BoxFit.cover,
+                    image: "https://flagcdn.com/w20/$country.png",
+                    imageErrorBuilder: (c, o, s) => Image.asset(
+                      "assets/images/placeholder.jpg",
+                      // fit: BoxFit.cover,
+                      height: 30,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   title: Text(
                     homeController.currenciesList[index].country,
                     style: const TextStyle(fontSize: 10),
                   ),
+                  trailing: Text(
+                    homeController.currenciesList[index].currency,
+                    style: const TextStyle(fontSize: 15),
+                  ),
+
                 );
               }
               return Container();
